030035b08343415edaac735be08cc9f8c47b0f34
jackjtaylor <jackjt@outlook.com.au>
2024-10-09 16:34:32 +0200

    refactor: add correct ResponseEntity and fix wrong function name

--- src/main/java/com/jackjtaylor/taskr/controllers/TaskController.java
+++ src/main/java/com/jackjtaylor/taskr/controllers/TaskController.java
@@ -1,5 +1,7 @@
 package com.jackjtaylor.taskr.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,8 @@ public class TaskController {
     }
 
     @PostMapping("/addTask")
-    public void postMethodName(@RequestBody TaskModel task) {
+    public ResponseEntity<HttpStatus> postTask(@RequestBody TaskModel task) {
         tasks.put(task.getTaskName(), task);
+        return ResponseEntity.accepted().build();
     }
 }
