02655197d0725f21d92ba6a340771852ef17c72f
jackjtaylor <jackjt@outlook.com.au>
2024-10-09 02:13:03 +0200

    feat: add Map<> and getTask() to TaskController, so TaskModels can be returned

--- src/main/java/com/jackjtaylor/taskr/controllers/TaskController.java
+++ src/main/java/com/jackjtaylor/taskr/controllers/TaskController.java
@@ -1,16 +1,27 @@
 package com.jackjtaylor.taskr.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jackjtaylor.taskr.model.TaskModel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class TaskController {
 
-    @GetMapping("/getTask")
-    public TaskModel getTask() {
-        return new TaskModel("Buy a MacBook", false);
+    private Map<String, TaskModel> tasks = new HashMap<>();
+
+    public TaskController() {
+        this.tasks.put("MacBook", new TaskModel("Buy a MacBook", false));
+        this.tasks.put("Headphones", new TaskModel("Look for Headphones", true));
+    }
+
+    @GetMapping("/getTask/{taskName}")
+    public TaskModel getTask(@PathVariable String taskName) {
+        return tasks.get(taskName);
     }
 
 }
